5c0903fc2a17a158131392771b736fa56d21865e
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-02 21:35:29 +0530

    Update file handling code and add new functionality.

--- Learn Node.js/learnFS/file.js
+++ Learn Node.js/learnFS/file.js
@@ -1,18 +1,26 @@
 const fs = require("fs");
-/*
-// fs.writeFileSync("./write.txt", "hello world"); //Sync
-// const result = fs.readFileSync("./read.txt", "utf-8"); //Sync
 
-fs.writeFile("./write.txt", "hello world Async", (err) => {}); //Async
+fs.writeFileSync("./write.txt", "hello world"); //Sync
+const result = fs.readFileSync("./read.txt", "utf-8"); //Sync
 console.log(result);
 
-fs.readFile("./read.txt", "utf-8", (err, data) => {
-    if (!err) {
-        console.log(data);
-    } else {
-        console.log(err);
-    }
-}); //Async
-*/
+fs.writeFile("./write.txt", "hello world Async", (err) => {}); //Async
 
-fs.appendFileSync("./append.txt", `${Date.now()}\n`);
+fs.readFile("./read.txt", "utf-8", (err, data) => {
+  if (!err) {
+    console.log(data);
+  } else {
+    console.log(err);
+  }
+}); //Async
+
+fs.appendFileSync("./append.txt", `${new Date().toISOString().split("T")}\n`);
+
+fs.cpSync("./append.txt", "./copy.txt");
+
+fs.unlinkSync("./copy.txt");
+
+console.log(fs.statSync("./read.txt"));
+
+fs.mkdirSync("single");
+fs.mkdirSync("dir/v/b", { recursive: true });
