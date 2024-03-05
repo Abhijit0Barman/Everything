777f42c31674827107dbbc48ff24d95a1ba97639
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-05 18:28:18 +0530

    Log IP address and request details to a file, serve dynamic responses based on the requested URL.

--- Learn Node.js/HTTP Server/index.js
+++ Learn Node.js/HTTP Server/index.js
@@ -1,19 +1,32 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-  const ipAddress = req.socket.remoteAddress;
-  const clientIP =
+  const ipAddress =
     req.headers["cf-connecting-ip"] ||
     req.headers["cf-connection-ip"] ||
     req.headers["x-real-ip"] ||
     req.headers["x-forwarded-for"] ||
     req.socket.remoteAddress ||
     null;
-  const clientIP2 = req.connection.remoteAddress;
   console.log(ipAddress);
-  console.log(clientIP);
-  console.log(clientIP2);
-  res.end("hello from server");
+
+  const log = `${Date.now()}: ${req.url} New Req Received ${ipAddress}\n`;
+  fs.appendFile("./log.txt", log, (err, data) => {
+    // console.log(err, "===>", data);
+    switch (req.url) {
+      case "/":
+        res.end("Homepate");
+        break;
+      case "/about":
+        res.end(`<b>about <h1>page</h1></b>`);
+        break;
+      default:
+        res.end("404 Not Found");
+    }
+  });
+
+  // res.end("hello from server");
 });
 
 myServer.listen(8000, () => {
