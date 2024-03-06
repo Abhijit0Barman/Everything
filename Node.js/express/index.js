6836b95cc68dee53323a5e2dbb82e99d689ca1a2
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-06 14:14:55 +0530

    SSR and CSR routes added.

--- Node.js/express/index.js
+++ Node.js/express/index.js
@@ -1,15 +1,39 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
-const server = express();
+const app = express();
+// app.use(express.json());
 
-server.use(express.json());
-//Routes
-server.get("/", (req, res) => {
-  if (req.url === "favicon.ico") return;
+//Routes SSR
+app.get("/users", (req, res) => {
+  const html = `
+  <ul>
+  ${users.map((user) => `<li> ${user.first_name} </li>`).join("")}
+}}
+</ul>
+`;
+  return res.send(html);
+});
+
+//Routes CSR
+app.get("/", (req, res) => {
+  // if (req.url === "favicon.ico") return;
   // res.status(200).json({message:"hello world"});
   res.status(200).end(`hello world`);
 });
 
-server.listen(8000, () => {
-  console.log(`server is running at 8000`);
+app.get("/api/users", (req, res) => {
+  return res.send(users);
+});
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.post("/api/users", (req, res) => {
+  return res.send({ post: "mehtod" });
+});
+app.listen(8000, () => {
+  console.log(`App is running at 8000`);
 });
