4bc24810f69c8081792c6ba6b53b0161db8de5e7
Abhijit0barman <abhijitbarman96@gmail.com>
2024-03-06 18:50:08 +0530

    ok

--- Node.js/express/index.js
+++ Node.js/express/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 
 const app = express();
-// app.use(express.json());
 
-//Routes SSR
+//MIDDLEWARE
+// app.use(express.json());
+//  or
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "./log.txt",
+    `${req.ip} ${new Date().toISOString()} ${req.method} ${req.url}||${
+      req.path
+    }\n`,
+    (err, data) => {}
+  );
+  next();
+});
+// app.use((req, res, next) => {
+//   console.log(`Middleware-2`);
+//   return res.end("bye bye")
+//   // next();
+// });
+
+//Routes ServerSideRendering
 app.get("/users", (req, res) => {
   const html = `
   <ul>
@@ -15,25 +36,73 @@ app.get("/users", (req, res) => {
   return res.send(html);
 });
 
-//Routes CSR
+//Routes ClientSideRendering
 app.get("/", (req, res) => {
   // if (req.url === "favicon.ico") return;
   // res.status(200).json({message:"hello world"});
-  res.status(200).end(`hello world`);
+  res.setHeader("devlopBy", "Abhijit");
+  return res.status(200).end(`hello world`);
 });
 
 app.get("/api/users", (req, res) => {
   return res.send(users);
 });
-app.get("/api/users/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const user = users.find((user) => user.id === id);
-  return res.json(user);
-});
 
 app.post("/api/users", (req, res) => {
-  return res.send({ post: "mehtod" });
+  const body = req.body;
+  // console.log(body);
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    return res.json({ post: "successfull", id: users.length });
+  });
+  // return res.json({ post: "mehtod" });
 });
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  });
+
 app.listen(8000, () => {
   console.log(`App is running at 8000`);
 });
+
+/* BECAUSE OF "SAME ROUTE" WE ARE GONNA MERGE TOGATHER
+
+    //GET request's
+    app.get("/api/users/:id", (req, res) => {
+      const id = Number(req.params.id);
+      const user = users.find((user) => user.id === id);
+      return res.json(user);
+    });
+    
+    //PATCH request's
+    app.patch("/api/users/:id", (req, res) => {
+      const id = Number(req.params.id);
+      const user = users.find((user) => user.id === id);
+      return res.json(user);
+    });
+    
+    //DELETE request's
+    app.delete("/api/users/:id", (req, res) => {
+      const id = Number(req.params.id);
+      const user = users.find((user) => user.id === id);
+      return res.json(user);
+    });
+    */
